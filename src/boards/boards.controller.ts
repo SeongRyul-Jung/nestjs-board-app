0af970294d16e0aad25dc7f1f90e0c9234080f1a
SR-Jung <westeeven0612@gmail.com>
2023-02-07 13:33:05 +0900

    id로 특정 게시물 지우기

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -21,8 +21,13 @@ export class BoardsController {
   }
 
   @Get('/:id')
-  getBoardById(@Param('id') id: string): Board{
+  getBoardById(@Param('id') id: string): Board {
     return this.borardsService.getBoardById(id);
+  }
+
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string): void {
+    this.borardsService.deleteBoard(id);
   }
 }
 
