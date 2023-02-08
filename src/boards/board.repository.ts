1d6a5d7f85d970f94c8166a03dbf35f214b4a87b
SR-Jung <westeeven0612@gmail.com>
2023-02-08 11:25:45 +0900

    ts, nestjs 버전문제

---
+++ src/boards/board.repository.ts
@@ -0,0 +1,8 @@
+import { EntityRepository, Repository } from "typeorm";
+import { Board } from "./board.entity";
+import { CustomRepository } from "./typeorm-ex.decorator";
+
+@CustomRepository(Board)
+export class BoardRepository extends Repository<Board> {
+
+}
--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,8 +1,16 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmExModule } from './typeorm-ex.module';
 import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
+import { BoardRepository } from './board.repository';
 
 @Module({
+  imports: [
+    TypeOrmExModule.forFeature([BoardRepository])
+    // TypeOrmModule.forFeature([BoardRepository])
+    // TypeOrmExModule.forFeature([forCustomRepository])
+  ],
   controllers: [BoardsController],
   providers: [BoardsService]
 })
